6ea4fc3897428d78f07d28c801c651b93fd4ba4e
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-07-19 02:34:32 -0400

    Adds font

--- functions.php
+++ functions.php
@@ -93,6 +93,7 @@ add_action( 'widgets_init', 'npmawesome_widgets_init' );
  */
 function npmawesome_scripts() {
   wp_enqueue_style( 'npmawesome-style', get_stylesheet_uri() );
+  wp_enqueue_style( 'npmawesome-fonts', 'http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,400italic' );
   wp_enqueue_script( 'npmawesome-jquery', '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js', array(), '2.0.3', true );
   wp_enqueue_script( 'npmawesome-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
   wp_enqueue_script( 'npmawesome-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
