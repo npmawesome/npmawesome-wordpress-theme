406076fc50e2f3f7815dbf40c5621d5bc54bf072
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-07-18 22:52:30 -0700

    Adds scripts

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function npmawesome_setup() {
 	register_nav_menus( array(
 		'primary' => __( 'Primary Menu', 'npmawesome' ),
 	) );
-	
+
 	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
@@ -93,10 +93,12 @@ add_action( 'widgets_init', 'npmawesome_widgets_init' );
  */
 function npmawesome_scripts() {
 	wp_enqueue_style( 'npmawesome-style', get_stylesheet_uri() );
-
 	wp_enqueue_script( 'npmawesome-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
 	wp_enqueue_script( 'npmawesome-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'npmawesome-numeral', get_template_directory_uri() . '/js/numeral.js', array(), '20140718', true );
+	wp_enqueue_script( 'npmawesome-jquery.unveil', get_template_directory_uri() . '/js/jquery.unveil.js', array(), '20140718', true );
+	wp_enqueue_script( 'npmawesome-github-stars', get_template_directory_uri() . '/js/github-stars.js', array(), '20140718', true );
+	wp_enqueue_script( 'npmawesome-main', get_template_directory_uri() . '/js/main.js', array(), '20140718', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
