b0edb538bc538218bd763afdc197a3247b59ff6f
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-07-22 07:19:02 -0700

    Fixes cards

--- content.php
+++ content.php
@@ -16,7 +16,7 @@ $classes = array(
 <article id="post-<?php the_ID(); ?>" <?php post_class($classes); ?>>
   <header>
     <h2>
-      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+      <a href="<?php the_permalink(); ?>" class="permalink"><?php the_title(); ?></a>
       <?php edit_post_link('Edit', '<span class="edit-link">', '</span>'); ?>
     </h2>
     <?php the_tags('<span class="meta-tags">', ', ', '</span>'); ?>
