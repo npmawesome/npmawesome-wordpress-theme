cc0c03b3e7dc2497c8e2b5bf19c0c120dad47f07
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-07-24 20:28:11 -0700

    Updates styling

--- content.php
+++ content.php
@@ -12,12 +12,12 @@ $classes = array(
   $is_module ? 'npm' : 'article'
 );
 
-if(has_post_thumbnail()) { 
+if(has_post_thumbnail()) {
   $post_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'medium');
   $post_image_url = $post_image_url[0];
   $post_image = "<div class='post-image' style='background-image: url($post_image_url);'><img src='$post_image_url'/></div>";
   array_push($classes, 'with-post-image');
-} 
+}
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class($classes); ?>>
@@ -31,11 +31,11 @@ if(has_post_thumbnail()) {
       ?>
       <h2>
         <a href="<?php the_permalink(); ?>" class="permalink"><?php the_title(); ?></a>
-        <?php edit_post_link('Edit', '<span class="edit-link">', '</span>'); ?>
       </h2>
       <?php echo npm_get_author(); ?>
       <?php // the_tags('<span class="meta-tags">', ', ', '</span>'); ?>
     </header>
+    <?php edit_post_link('Edit', '<span class="edit-link">', '</span>'); ?>
     <?php echo $post_image; ?>
   </div>
 </div>
