5960785daf62a242b4be3454ae02625027871e61
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-07-28 19:52:22 -0700

    Moved out GA code.

--- footer.php
+++ footer.php
@@ -22,17 +22,9 @@
   <a class="hide-on-mobile" id="fork-me" href="https://github.com/npmawesome">Fork Me</a>
 
   <script tyle="text/javascript">
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
     // http://drawingablank.me/blog/fix-your-bounce-rate.html
     // https://news.ycombinator.com/item?id=5766883
     setTimeout(function() { ga('send', 'event', 'read', 'auto generated'); }, 5000);
-
-    ga('create', 'UA-46164085-1', 'npmawesome.com');
-    ga('send', 'pageview');
   </script>
 
   <script async src="//platform.twitter.com/widgets.js"></script>
