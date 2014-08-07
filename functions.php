2cadcccaab04a923c1f46c885c1b5624986b5938
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-08-07 14:15:18 -0400

    Enables SVG image upload

--- functions.php
+++ functions.php
@@ -5,6 +5,16 @@
  * @package npmawesome
  */
 
+add_filter('upload_mimes', 'custom_upload_mimes');
+
+function custom_upload_mimes($existing_mimes = array()) {
+  // add the file extension to the array
+  $existing_mimes['svg'] = 'mime/type';
+
+  // call the modified list of extensions
+  return $existing_mimes;
+}
+
 /**
  * Set the content width based on the theme's design and stylesheet.
  */
